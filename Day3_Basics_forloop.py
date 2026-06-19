b0930fc114dbeffe7b5c025a1489eb0adc0ead5b
hunter20020511 <nachiketkulkarni@orchidengg.ac.in>
2026-06-19 05:58:50 +0000

    Practiced for loops

---
+++ Day3_Basics_forloop.py
@@ -0,0 +1,7 @@
+numbers=[45,56,36,50,99,20]
+for i in numbers:
+    print(i) #prints all numbers from list -> numbers
+for numbers in range(10):
+    print(numbers)
+for numbers in range(1, 11):
+    print(numbers)
